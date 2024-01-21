ca978f91060ca01331c6bf1a942a0dd59e43a028
rafaqz <rafaelschouten@gmail.com>
2024-01-21 10:17:49 +0100

    fix tests

--- test/groupby.jl
+++ test/groupby.jl
@@ -7,5 +7,6 @@ const DD = DimensionalData
 # Make a demo DimArray
 A = rand(X(1:0.1:2), Y(1:20), Ti(DateTime(2000):Day(10):DateTime(2003)))
 # Group by month and even/odd Y axis values
-groupmeans = groupby(A, Ti=month, Y=isodd)
-groupmeans = mean.(groupby(A, Ti=month, Y=isodd))
+groupmeans = DimensionalData.groupby(A, Ti=>month, Y=>isodd)
+groupmeans = mean.(DimensionalData.groupby(A, Ti=>month, Y=>isodd))
+
